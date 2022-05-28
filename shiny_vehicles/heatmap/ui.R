c7204c06514081db8c23e34b3996bc7ed9d3f534
DhiaS-MSukor <dhiasyahmie@gmail.com>
2022-05-28 14:37:33 +0800

    new plots

--- shiny_vehicles/heatmap/ui.R
+++ shiny_vehicles/heatmap/ui.R
@@ -125,6 +125,29 @@ grouped_barplot <- fluidPage(
   )
 )
 
+# page for price of type of vehicles by year
+type_lineplot <- fluidPage( 
+  titlePanel("Prices of used cars over time"),
+  sidebarLayout(
+    sidebarPanel( 
+      checkboxGroupInput(inputId = "type_tl",
+                         label = "Choose type",
+                         choices = choice.type[choice.type!='all'],
+                         selected = c('offroad', 'hatchback', 'coupe', 'other', 'wagon', 'bus'))
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Medians", plotOutput('type_line')),
+        tabPanel("Medians against all types", plotOutput('type_facetline')),
+        tabPanel("Linear smoothed", plotOutput("type_lm")),
+        tabPanel("Generalized additive smoothed", plotOutput("type_gam"))
+      ) 
+    )
+  )
+)
+
 # main page
 main_page <- fluidPage(
   titlePanel("Shiny Used Car"),
@@ -134,7 +157,8 @@ main_page <- fluidPage(
     tabPanel("Used car availability heatmap", used_car_heatmap),
     tabPanel("Most available used car", available_barchart), 
     tabPanel("Price of top 20 most common used cars", price_boxwhisker), 
-    tabPanel("Prices of cars for each type of fuel", grouped_barplot)
+    tabPanel("Prices of cars for each type of fuel", grouped_barplot),
+    tabPanel("Prices of used cars over time", type_lineplot)
   )
 )
 
